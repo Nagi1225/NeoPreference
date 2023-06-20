f229ef962611cc5a600ac569d0da60e4ee829af6
Nagi1225 <spectre1225@163.com>
2023-06-20 08:56:25 +0800

    replace WeakHashMap with HashMap, remove useless code

--- NeoPreference/src/main/java/com/nagi/neopreference/PropertyFactory.java
+++ NeoPreference/src/main/java/com/nagi/neopreference/PropertyFactory.java
@@ -36,7 +36,7 @@ class LazyPropertyFactory<T> implements PropertyFactory<T> {
         return property;
     }
 
-    static PropertyFactory<?> get(Method method) {
+    static PropertyFactory<?> get(String preferenceName, Method method) {
         if (method.getParameterTypes().length == 0) {
             Type returnType = method.getGenericReturnType();
             if (returnType instanceof ParameterizedType) {
@@ -47,28 +47,28 @@ class LazyPropertyFactory<T> implements PropertyFactory<T> {
                     checkAnnotation(method, valueType, method.getAnnotations());
                     return new LazyPropertyFactory(Matcher.matchFrom(valueType)
                             .valueOf(Integer.class, intClass -> (PropertyFactory)
-                                    preferences -> new Property.IntProperty(defaultKey, extractAnnotation(method, Config.IntItem.class), preferences))
+                                    preferences -> new Property.IntProperty(defaultKey, extractAnnotation(method, Config.IntItem.class), preferenceName, preferences))
                             .valueOf(Float.class, floatClass ->
-                                    preferences -> new Property.FloatProperty(defaultKey, extractAnnotation(method, Config.FloatItem.class), preferences))
+                                    preferences -> new Property.FloatProperty(defaultKey, extractAnnotation(method, Config.FloatItem.class), preferenceName, preferences))
                             .valueOf(String.class, stringClass ->
-                                    preferences -> new Property.StringProperty(defaultKey, extractAnnotation(method, Config.StringItem.class), preferences))
+                                    preferences -> new Property.StringProperty(defaultKey, extractAnnotation(method, Config.StringItem.class), preferenceName, preferences))
                             .valueOf(Boolean.class, booleanClass ->
-                                    preferences -> new Property.BooleanProperty(defaultKey, extractAnnotation(method, Config.BooleanItem.class), preferences))
+                                    preferences -> new Property.BooleanProperty(defaultKey, extractAnnotation(method, Config.BooleanItem.class), preferenceName, preferences))
                             .valueOf(Long.class, longClass ->
-                                    preferences -> new Property.LongProperty(defaultKey, extractAnnotation(method, Config.LongItem.class), preferences))
+                                    preferences -> new Property.LongProperty(defaultKey, extractAnnotation(method, Config.LongItem.class), preferenceName, preferences))
                             .valueOf(Serializable.class, serializableClass ->
-                                    preferences -> new Property.SerializableProperty<>(defaultKey, extractAnnotation(method, Config.SerializableItem.class), preferences))
+                                    preferences -> new Property.SerializableProperty<>(defaultKey, extractAnnotation(method, Config.SerializableItem.class), preferenceName, preferences))
                             .typeOf(ParameterizedType.class, parameterizedType -> {
                                 Type rawType = ((ParameterizedType) valueType).getRawType();
                                 Type[] actualTypeArguments = ((ParameterizedType) valueType).getActualTypeArguments();
                                 if (Objects.equals(rawType, Set.class) && Objects.equals(actualTypeArguments[0], String.class)) {
-                                    return preferences -> new Property.StringSetProperty(defaultKey, extractAnnotation(method, Config.StringSetItem.class), preferences);
+                                    return preferences -> new Property.StringSetProperty(defaultKey, extractAnnotation(method, Config.StringSetItem.class), preferenceName, preferences);
                                 } else {
                                     throw new RuntimeException("error parameterizedType:" + rawType + " - " + Arrays.toString(actualTypeArguments));
                                 }
                             })
                             .caseOf(type -> type instanceof Class && Serializable.class.isAssignableFrom((Class<?>) type), type ->
-                                    preferences -> new Property.SerializableProperty<>(defaultKey, extractAnnotation(method, Config.SerializableItem.class), preferences))
+                                    preferences -> new Property.SerializableProperty<>(defaultKey, extractAnnotation(method, Config.SerializableItem.class), preferenceName, preferences))
                             .orElse(() -> {
                                 throw new RuntimeException("error returnType:" + valueType);
                             }));
